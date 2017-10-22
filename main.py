a2302ea411af082157a1a7c4906fe43e06746b7e
jackoe <jackwines@mac.com>
2017-10-21 20:04:10 -0500

    added lines

---
+++ main.py
@@ -0,0 +1,15 @@
+import nltk
+lines = ["I hate this.", 
+    "Running is terrible.", 
+    "Everything is the worst.", 
+    "Sometimes I feel like I was born with a leak.", 
+    "Any goodness I started with just slowly spilled out of me.", 
+    "Now it is all gone.", 
+    "You didn’t know me .", 
+    "Then you fell in love with me.", 
+    "Now you know me.", 
+    "I need to go take a shower.", 
+    "I can not tell if I am crying.", 
+    "I just spent 7 hours playing with fonts."]
+
+print(lines[0])
