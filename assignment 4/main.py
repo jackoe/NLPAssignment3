d37c2b2815ac932af227b5ba3693904ca1aa91ea
jackoe <jackwines@mac.com>
2017-10-31 19:15:24 -0500

    added get50Pairs

--- assignment 4/main.py
+++ assignment 4/main.py
@@ -7,3 +7,15 @@ def magnitude(vec):
 def normalize(vec):
     vecMagnitude = getMagnitude(vec)
     return [x / vecMagnitude for x in vec]
+
+def get50Pairs():
+    pairs = None
+    with open('50pairs.txt', 'r') as f:
+        lines = list(f)
+        halfway = len(lines) // 2
+        print(halfway)
+        pairs = zip(lines[:halfway], lines[halfway:])
+    return pairs
+
+
+
