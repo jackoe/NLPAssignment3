6199aef3ee15b8ab653efdbe568dee7e5a7b5b78
jackoe <jackwines@mac.com>
2017-10-31 19:21:58 -0500

    get50Pairs has been debugged

--- assignment 4/main.py
+++ assignment 4/main.py
@@ -1,3 +1,6 @@
+# Kerim Celik
+# Jack Wines
+
 def dotProduc(vec1, vec2):
     return sum([x * y for x,y in zip(vec1, vec2)])
 
@@ -8,14 +11,19 @@ def normalize(vec):
     vecMagnitude = getMagnitude(vec)
     return [x / vecMagnitude for x in vec]
 
+def parseLine(line):
+    l = line.split()
+    return [l[0]] + [float(x) for x in l[1:]]
+
 def get50Pairs():
     pairs = None
     with open('50pairs.txt', 'r') as f:
         lines = list(f)
+        lines = [parseLine(line) for line in lines]
         halfway = len(lines) // 2
         print(halfway)
         pairs = zip(lines[:halfway], lines[halfway:])
     return pairs
 
 
-
+print(list(get50Pairs()))
